d0369d349a027c0483af718f6347e0e9f98b7f57
leemon <tacrapo@gmail.com>
2016-12-11 14:39:48 +0100

    First commit

---
+++ woocommerce-author-taxonomy.php
@@ -0,0 +1,383 @@
+<?php
+/*
+Plugin Name: WooCommerce Author Taxonomy
+Plugin URI: 
+Description: Adds an author taxonomy to products.
+Version: 1.0
+Author: Oscar Ciutat
+Author URI: http://oscarciutat.com/code/
+Text Domain: woocommerce-author-taxonomy
+License: GPLv2 or later
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
+class WooCommerce_Author_Taxonomy {
+	
+	/*
+	 * constructor
+	 *
+	 * @since 1.0
+	 */
+    function __construct() {
+
+		// Init
+        add_action( 'init', array($this, 'init') );
+        add_action( 'admin_init', array($this, 'admin_init') );
+        add_action( 'admin_init', array($this, 'save_admin_settings'), 0 );
+		add_filter( 'template_include', array($this, 'template_include') );
+
+		// Add form
+		add_action( 'product_author_add_form_fields', array( $this, 'add_author_fields' ) );
+		add_action( 'product_author_edit_form_fields', array( $this, 'edit_author_fields' ), 10 );
+		add_action( 'created_term', array( $this, 'save_author_fields' ), 10, 3 );
+		add_action( 'edit_term', array( $this, 'save_author_fields' ), 10, 3 );
+		
+		// Add columns
+		add_filter( 'manage_edit-product_author_columns', array( $this, 'product_author_columns' ) );
+		add_filter( 'manage_product_author_custom_column', array( $this, 'product_author_column' ), 10, 3 );
+		
+    }
+    
+    
+	/*
+	 * init
+	 *
+	 * @since 1.0
+	 */
+    function init() {
+		load_plugin_textdomain( 'woocommerce-author-taxonomy', '', dirname(plugin_basename( __FILE__ )) . '/languages/' );
+		$this->register_taxonomy();
+    }
+
+    
+	/*
+	 * admin_init
+	 *
+	 * @since 1.0
+	 */
+    function admin_init() {
+		add_settings_field(
+			'woocommerce_product_product_author_slug',
+			__( 'Product Author base', 'woocommerce-author-taxonomy' ),
+			array($this, 'product_tax_slug_input'),
+			'permalink',
+			'optional',
+			'product_author'
+		); 
+    }
+    
+	
+    /*
+	 * product_tax_slug_input
+	 *
+	 * @since 1.0
+	 */
+	function product_tax_slug_input( $taxonomy_slug ) {
+        $permalinks = get_option( 'wat_permalinks' );
+	?>
+		<input name="wc_product_author_slug" type="text" class="regular-text code" value="<?php if ( isset( $permalinks['product_author_tax_base'] ) ) echo esc_attr( $permalinks['product_author_tax_base'] ); ?>" placeholder="<?php echo _x('product-author', 'slug', 'woocommerce-author-taxonomy') ?>" />
+	<?php
+    }
+    
+	
+    /*
+	 * save_admin_settings
+	 *
+	 * @since 1.0
+	 */
+    function save_admin_settings() {
+        if ( ! is_admin() ) {
+            return;
+		}
+		
+        $permalinks = array();
+        
+		if ( isset( $_POST['wc_product_author_slug'] ) ) {
+			$permalinks['product_author_tax_base'] = untrailingslashit(woocommerce_clean($_POST['wc_product_author_slug']));
+        }
+        
+        if ( !empty( $permalinks ) ) {
+            update_option( 'wat_permalinks', $permalinks );
+            flush_rewrite_rules();   
+        }
+    }
+
+	
+    /*
+	 * register_taxonomy
+	 *
+	 * @since 1.0
+	 */
+    function register_taxonomy() {
+		$permalinks = get_option( 'wat_permalinks' );
+        $product_tax_slug = empty( $permalinks['product_author_tax_base'] ) ? 'product_author' : $permalinks['product_author_tax_base'];
+        
+        $labels = array(
+			'name' => __( 'Authors', 'woocommerce-author-taxonomy' ),
+            'singular_name' => __( 'Author', 'woocommerce-author-taxonomy' ),
+			'search_items' => __( 'Search Author', 'woocommerce-author-taxonomy' ),
+			'all_items' => __( 'All Authors', 'woocommerce-author-taxonomy' ),
+            'parent_item' => __( 'Parent Author', 'woocommerce-author-taxonomy' ),
+            'parent_item_colon' => __( 'Parent Author:', 'woocommerce-author-taxonomy' ),
+            'edit_item' => __( 'Edit Author', 'woocommerce-author-taxonomy' ),
+            'view_item' => __( 'View Author', 'woocommerce-author-taxonomy' ),
+            'update_item' => __( 'Update Author', 'woocommerce-author-taxonomy' ),
+            'add_new_item' => __( 'Add New Author', 'woocommerce-author-taxonomy' ),
+            'new_item_name' => __( 'New Author Name', 'woocommerce-author-taxonomy' ),
+            'not_found' => __( 'No authors found', 'woocommerce-author-taxonomy' ),
+            'no_terms' => __( 'No authors', 'woocommerce-author-taxonomy' ),
+			'items_list_navigation' => __( 'Authors list navigation', 'woocommerce-author-taxonomy' ),
+			'items_list' => __( 'Authors list', 'woocommerce-author-taxonomy' )
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'hierarchical' => true,
+            'public' => true,
+            'show_ui' => true,
+            'show_admin_column' => true,
+            'show_in_nav_menus' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => $product_tax_slug,
+                'with_front' => false,
+                'hierarchical' => true
+            )
+        );
+		
+        register_taxonomy( 'product_author', 'product', $args );
+    }
+	
+	
+    /*
+	 * add_author_fields
+	 *
+	 * @since 1.0
+	 */
+	public function add_author_fields() {
+		?>
+		<div class="form-field term-thumbnail-wrap">
+			<label><?php _e( 'Thumbnail', 'woocommerce' ); ?></label>
+			<div id="product_author_thumbnail" style="float: left; margin-right: 10px;"><img src="<?php echo esc_url( wc_placeholder_img_src() ); ?>" width="60px" height="60px" /></div>
+			<div style="line-height: 60px;">
+				<input type="hidden" id="product_author_thumbnail_id" name="product_author_thumbnail_id" />
+				<button type="button" class="upload_image_button button"><?php _e( 'Upload/Add image', 'woocommerce' ); ?></button>
+				<button type="button" class="remove_image_button button"><?php _e( 'Remove image', 'woocommerce' ); ?></button>
+			</div>
+			<script type="text/javascript">
+				// Only show the "remove image" button when needed
+				if ( ! jQuery( '#product_author_thumbnail_id' ).val() ) {
+					jQuery( '.remove_image_button' ).hide();
+				}
+				// Uploading files
+				var file_frame;
+				jQuery( document ).on( 'click', '.upload_image_button', function( event ) {
+					event.preventDefault();
+					// If the media frame already exists, reopen it.
+					if ( file_frame ) {
+						file_frame.open();
+						return;
+					}
+					// Create the media frame.
+					file_frame = wp.media.frames.downloadable_file = wp.media({
+						title: '<?php _e( "Choose an image", "woocommerce" ); ?>',
+						button: {
+							text: '<?php _e( "Use image", "woocommerce" ); ?>'
+						},
+						multiple: false
+					});
+					// When an image is selected, run a callback.
+					file_frame.on( 'select', function() {
+						var attachment = file_frame.state().get( 'selection' ).first().toJSON();
+						jQuery( '#product_author_thumbnail_id' ).val( attachment.id );
+						jQuery( '#product_author_thumbnail' ).find( 'img' ).attr( 'src', attachment.sizes.thumbnail.url );
+						jQuery( '.remove_image_button' ).show();
+					});
+					// Finally, open the modal.
+					file_frame.open();
+				});
+				jQuery( document ).on( 'click', '.remove_image_button', function() {
+					jQuery( '#product_author_thumbnail' ).find( 'img' ).attr( 'src', '<?php echo esc_js( wc_placeholder_img_src() ); ?>' );
+					jQuery( '#product_author_thumbnail_id' ).val( '' );
+					jQuery( '.remove_image_button' ).hide();
+					return false;
+				});
+				jQuery( document ).ajaxComplete( function( event, request, options ) {
+					if ( request && 4 === request.readyState && 200 === request.status
+						&& options.data && 0 <= options.data.indexOf( 'action=add-tag' ) ) {
+						var res = wpAjax.parseAjaxResponse( request.responseXML, 'ajax-response' );
+						if ( ! res || res.errors ) {
+							return;
+						}
+						// Clear Thumbnail fields on submit
+						jQuery( '#product_author_thumbnail' ).find( 'img' ).attr( 'src', '<?php echo esc_js( wc_placeholder_img_src() ); ?>' );
+						jQuery( '#product_author_thumbnail_id' ).val( '' );
+						jQuery( '.remove_image_button' ).hide();
+						// Clear Display type field on submit
+						jQuery( '#display_type' ).val( '' );
+						return;
+					}
+				} );
+			</script>
+			<div class="clear"></div>
+		</div>
+		<?php
+	}	
+	
+	
+    /*
+	 * edit_author_fields
+	 *
+	 * @since 1.0
+	 */
+	public function edit_author_fields( $term ) {
+		$thumbnail_id = absint( get_woocommerce_term_meta( $term->term_id, 'thumbnail_id', true ) );
+		if ( $thumbnail_id ) {
+			$image = wp_get_attachment_thumb_url( $thumbnail_id );
+		} else {
+			$image = wc_placeholder_img_src();
+		}
+		?>
+		<tr class="form-field">
+			<th scope="row" valign="top"><label><?php _e( 'Thumbnail', 'woocommerce' ); ?></label></th>
+			<td>
+				<div id="product_author_thumbnail" style="float: left; margin-right: 10px;"><img src="<?php echo esc_url( $image ); ?>" width="60px" height="60px" /></div>
+				<div style="line-height: 60px;">
+					<input type="hidden" id="product_author_thumbnail_id" name="product_author_thumbnail_id" value="<?php echo $thumbnail_id; ?>" />
+					<button type="button" class="upload_image_button button"><?php _e( 'Upload/Add image', 'woocommerce' ); ?></button>
+					<button type="button" class="remove_image_button button"><?php _e( 'Remove image', 'woocommerce' ); ?></button>
+				</div>
+				<script type="text/javascript">
+					// Only show the "remove image" button when needed
+					if ( '0' === jQuery( '#product_author_thumbnail_id' ).val() ) {
+						jQuery( '.remove_image_button' ).hide();
+					}
+					// Uploading files
+					var file_frame;
+					jQuery( document ).on( 'click', '.upload_image_button', function( event ) {
+						event.preventDefault();
+						// If the media frame already exists, reopen it.
+						if ( file_frame ) {
+							file_frame.open();
+							return;
+						}
+						// Create the media frame.
+						file_frame = wp.media.frames.downloadable_file = wp.media({
+							title: '<?php _e( "Choose an image", "woocommerce" ); ?>',
+							button: {
+								text: '<?php _e( "Use image", "woocommerce" ); ?>'
+							},
+							multiple: false
+						});
+						// When an image is selected, run a callback.
+						file_frame.on( 'select', function() {
+							var attachment = file_frame.state().get( 'selection' ).first().toJSON();
+							jQuery( '#product_author_thumbnail_id' ).val( attachment.id );
+							jQuery( '#product_author_thumbnail' ).find( 'img' ).attr( 'src', attachment.sizes.thumbnail.url );
+							jQuery( '.remove_image_button' ).show();
+						});
+						// Finally, open the modal.
+						file_frame.open();
+					});
+					jQuery( document ).on( 'click', '.remove_image_button', function() {
+						jQuery( '#product_author_thumbnail' ).find( 'img' ).attr( 'src', '<?php echo esc_js( wc_placeholder_img_src() ); ?>' );
+						jQuery( '#product_author_thumbnail_id' ).val( '' );
+						jQuery( '.remove_image_button' ).hide();
+						return false;
+					});
+				</script>
+				<div class="clear"></div>
+			</td>
+		</tr>
+		<?php
+	}	
+	
+	
+    /*
+	 * save_author_fields
+	 *
+	 * @since 1.0
+	 */
+	public function save_author_fields( $term_id, $tt_id = '', $taxonomy = '' ) {
+		if ( isset( $_POST['product_author_thumbnail_id'] ) && 'product_author' === $taxonomy ) {
+			update_woocommerce_term_meta( $term_id, 'thumbnail_id', absint( $_POST['product_author_thumbnail_id'] ) );
+		}
+	}
+	
+	
+    /*
+	 * product_author_columns
+	 *
+	 * @since 1.0
+	 */
+	public function product_author_columns( $columns ) {
+		$new_columns = array();
+		if ( isset( $columns['cb'] ) ) {
+			$new_columns['cb'] = $columns['cb'];
+			unset( $columns['cb'] );
+		}
+		$new_columns['thumb'] = __( 'Image', 'woocommerce' );
+		return array_merge( $new_columns, $columns );
+	}
+	
+	
+    /*
+	 * product_author_column
+	 *
+	 * @since 1.0
+	 */
+	public function product_author_column( $columns, $column, $id ) {
+		if ( 'thumb' == $column ) {
+			$thumbnail_id = get_woocommerce_term_meta( $id, 'thumbnail_id', true );
+			if ( $thumbnail_id ) {
+				$image = wp_get_attachment_thumb_url( $thumbnail_id );
+			} else {
+				$image = wc_placeholder_img_src();
+			}
+			// Prevent esc_url from breaking spaces in urls for image embeds
+			// Ref: https://core.trac.wordpress.org/ticket/23605
+			$image = str_replace( ' ', '%20', $image );
+			$columns .= '<img src="' . esc_url( $image ) . '" alt="' . esc_attr__( 'Thumbnail', 'woocommerce' ) . '" class="wp-post-image" height="48" width="48" />';
+		}
+		return $columns;
+	}
+	
+    /*
+	 * template_include
+	 *
+	 * @since 1.0
+	 */
+	function template_include( $template ) {
+		global $post;
+
+		if (is_tax('product_author')) {
+			if ( $file = locate_template( array( 'taxonomy-product_author.php' ) ) ) {
+				$template = $file;
+			} else {
+				$template = plugin_dir_path(__FILE__) . '/templates/taxonomy-product_author.php';
+			}
+		}
+
+		return $template;
+	}
+
+	
+}
+
+$woocommerce_author_taxonomy = new WooCommerce_Author_Taxonomy;
+
+}
