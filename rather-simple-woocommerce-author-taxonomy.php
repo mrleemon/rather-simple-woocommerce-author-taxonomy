581685df7db986f0ce027e12b348011326f4baba
leemon <tacrapo@gmail.com>
2020-06-18 11:34:53 +0200

    Update "WC tested up to" tag

--- rather-simple-woocommerce-author-taxonomy.php
+++ rather-simple-woocommerce-author-taxonomy.php
@@ -4,7 +4,7 @@ Plugin Name: Rather Simple WooCommerce Author Taxonomy
 Plugin URI: 
 Description: Adds an author taxonomy to products.
 Version: 1.0
-WC tested up to: 3.6.5
+WC tested up to: 4.2
 Author: Oscar Ciutat
 Author URI: http://oscarciutat.com/code/
 Text Domain: rather-simple-woocommerce-author-taxonomy
